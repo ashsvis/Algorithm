54b8c991c9173a9e76f2d0fdba8fb8662d2cc02b
ashsvis <ashsvis@gmail.com>
2023-01-12 15:44:31 +0300

    Контекстное меню

--- AlgorithmViewer/AlgorithmUC.Designer.cs
+++ AlgorithmViewer/AlgorithmUC.Designer.cs
@@ -28,7 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.figureContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiAddFigure = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiRemoveFigure = new System.Windows.Forms.ToolStripMenuItem();
+            this.figureContextMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // figureContextMenu
+            // 
+            this.figureContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiAddFigure,
+            this.tsmiRemoveFigure});
+            this.figureContextMenu.Name = "figureContextMenu";
+            this.figureContextMenu.Size = new System.Drawing.Size(181, 70);
+            // 
+            // tsmiAddFigure
+            // 
+            this.tsmiAddFigure.Name = "tsmiAddFigure";
+            this.tsmiAddFigure.Size = new System.Drawing.Size(180, 22);
+            this.tsmiAddFigure.Text = "Добавить фигуру";
+            // 
+            // tsmiRemoveFigure
+            // 
+            this.tsmiRemoveFigure.Name = "tsmiRemoveFigure";
+            this.tsmiRemoveFigure.Size = new System.Drawing.Size(180, 22);
+            this.tsmiRemoveFigure.Text = "Удалить фигуру";
             // 
             // AlgorithmUC
             // 
@@ -37,10 +62,15 @@
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.Name = "AlgorithmUC";
             this.Size = new System.Drawing.Size(329, 319);
+            this.figureContextMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip figureContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAddFigure;
+        private System.Windows.Forms.ToolStripMenuItem tsmiRemoveFigure;
     }
 }
--- AlgorithmViewer/AlgorithmUC.cs
+++ AlgorithmViewer/AlgorithmUC.cs
@@ -17,8 +17,13 @@ namespace AlgorithmViewer
         {
             InitializeComponent();
             SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint | ControlStyles.ResizeRedraw, true);
+            figureContextMenu.Opening += FigureContextMenu_Opening;
+            tsmiAddFigure.Click += TsmiAddFigure_Click;
+            tsmiRemoveFigure.Click += TsmiRemoveFigure_Click;
         }
 
+        private Point lastMouseLocation = Point.Empty;
+        private Figure figure;
         private bool contentChanged = false;
         private int lastVertexIndex;
         private Algorithm.Size shiftSize = new Algorithm.Size(0, 0);
@@ -46,25 +51,23 @@ namespace AlgorithmViewer
         protected override void OnMouseDown(MouseEventArgs e)
         {
             base.OnMouseDown(e);
+            lastMouseLocation = e.Location;
+            figure = figures.FindLast(fig => GraphicsHelper.Contains(fig, e.Location));
+            if (figure != null)
+            {
+                lastVertexIndex = figures.FindLastIndex(v => v.Location.Equals(figure.Location));
+                shiftSize.Width = figure.Location.X - e.Location.X;
+                shiftSize.Height = figure.Location.Y - e.Location.Y;
+            }
+            else
+                lastVertexIndex = -1;
             if (e.Button == MouseButtons.Left)
             {
                 buttonPressed = e.Button == MouseButtons.Left;
                 contentChanged = false;
-                // ищем вершину с координатами рядом с точкой нажатия
-                var figure = figures.FindLast(fig => GraphicsHelper.Contains(fig, e.Location));
-                if (figure == null) 
-                {
-                    figure = new Figure() { Location = new Location(e.Location.X, e.Location.Y) };
-                    figures.Add(figure);
-                    Invalidate();
-                }
-                else
-                {
-                    lastVertexIndex = figures.FindLastIndex(v => v.Location.Equals(figure.Location));
-                    shiftSize.Width = figure.Location.X - e.Location.X;
-                    shiftSize.Height = figure.Location.Y - e.Location.Y;
-                }
             }
+            else
+                figureContextMenu.Show(this, e.Location);
         }
 
         /// <summary>
@@ -74,18 +77,15 @@ namespace AlgorithmViewer
         protected override void OnMouseMove(MouseEventArgs e)
         {
             base.OnMouseMove(e);
-            if (figures.Count > 0)
+            Cursor = figures.Any(fig => GraphicsHelper.Contains(fig, e.Location)) ? Cursors.Hand : Cursors.Default;
+            if (buttonPressed)
             {
-                Cursor = figures.Any(fig => GraphicsHelper.Contains(fig, e.Location)) ? Cursors.Hand : Cursors.Default;
-                if (buttonPressed)
+                if (lastVertexIndex >= 0 && lastVertexIndex < figures.Count)
                 {
-                    if (lastVertexIndex >= 0 && lastVertexIndex < figures.Count)
-                    {
-                        // перемещаем одну вершину
-                        figures[lastVertexIndex].Location = new Location(e.Location.X + shiftSize.Width, e.Location.Y + shiftSize.Height);
-                        contentChanged = true;
-                        Invalidate();
-                    }
+                    // перемещаем одну вершину
+                    figures[lastVertexIndex].Location = new Location(e.Location.X + shiftSize.Width, e.Location.Y + shiftSize.Height);
+                    contentChanged = true;
+                    Invalidate();
                 }
             }
         }
@@ -98,6 +98,28 @@ namespace AlgorithmViewer
         {
             base.OnMouseUp(e);
             buttonPressed = false;
+        }
+
+        private void FigureContextMenu_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            tsmiAddFigure.Visible = figure == null;
+            tsmiRemoveFigure.Visible = figure != null;
+        }
+
+        private void TsmiAddFigure_Click(object sender, EventArgs e)
+        {
+            var figure = new Figure() { Location = new Location(lastMouseLocation.X, lastMouseLocation.Y) };
+            figures.Add(figure);
+            Invalidate();
+        }
+
+        private void TsmiRemoveFigure_Click(object sender, EventArgs e)
+        {
+            if (figure != null)
+            {
+                figures.Remove(figure);
+                Invalidate();
+            }
         }
     }
 }
